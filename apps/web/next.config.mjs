0f95971f2d9618333ed5629063b7fd006c761153
zhuguibiao <zhuguibiao1994@gmail.com>
2025-11-14 01:42:23 +0800

    chore: workflow

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
+console.log("PAGES_BASE_PATH:", process.env.PAGES_BASE_PATH);
 const nextConfig = {
   transpilePackages: ["@labelz/ui"],
   reactStrictMode: false,
   output: "export",
   basePath: process.env.PAGES_BASE_PATH,
+  
   eslint: {
     ignoreDuringBuilds: true,
     dirs: [
